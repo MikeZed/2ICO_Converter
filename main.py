b3b9a0604cb9d9205de10376561bb971bd71f108
MikeZed <39437367+MikeZed@users.noreply.github.com>
2020-05-14 18:33:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+
+folder_to_convert = ''
+destination = ''
+
+
+def main():
+
+    # if destination doesnt exist create folder
+
+    # check files in folder, open, convert, save
+
+    # add converting class
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='main':
+    main()
