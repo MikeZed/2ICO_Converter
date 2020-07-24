20692b267e5dee4ad6ab6de8d042e789ec9645eb
MikeZed <39437367+MikeZed@users.noreply.github.com>
2020-07-24 14:46:27 +0300

    Added general image format converting option.

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import os
 
 source = r"Icons\To Be Converted"
 destination = r"Icons"
+destination_format=".png"
 
 
 def main():
 
-    img_converter = ImageConverter(source, destination)
+    img_converter = ImageConverter(source, destination, destination_format)
     img_converter.convert()
 
 
 class ImageConverter:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+    def __init__(self, src, dest, dest_format=".ico"):
+        self.source = src
+        self.destination = dest
+        self.destination_format = dest_format
 
     def convert(self):
         img_list = os.listdir(self.source)
@@ -24,7 +26,7 @@ class ImageConverter:
             img_name = img_file.split('.')[0]
 
             img=Image.open(self.source+"\\"+img_file)
-            img.save(self.destination+"\\"+img_name+".ico")
+            img.save(self.destination+"\\"+img_name + self.destination_format)
 
 
 if __name__ == "__main__":
